cc7dcfc65de496b541d7e6a803481ed2d918ca61
Infernio <infernio@icloud.com>
2021-11-13 11:18:28 +0100

    Bump ANTLR test version

--- scripts/prereq.sh
+++ scripts/prereq.sh
@@ -2,4 +2,4 @@
 # This script downloads all build prerequisites for wizparse.
 
 # Currently only the latest ANTLR release.
-curl https://www.antlr.org/download/antlr-4.8-complete.jar --output scripts/antlr.jar
+curl https://www.antlr.org/download/antlr-4.9.3-complete.jar --output scripts/antlr.jar
